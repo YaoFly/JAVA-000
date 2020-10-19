0d0c86aa9aca414edff0e20ac78e28b284c3dcf1
wangyaohui <wangyaohui@vv.cn>
2020-10-19 22:56:20 +0800

    自定义Classloader 加载Hello.java 并执行指定方法

---
+++ Week_01/CustomClassloader.java
@@ -0,0 +1,44 @@
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class CustomClassloader extends ClassLoader {
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        Class<?> cls = findLoadedClass(name);
+        if (cls != null) {
+            return cls;
+        }
+
+        String path = "";
+        try {
+            InputStream is = new FileInputStream(path+name+".xlass");
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            int b;
+            while ((b = is.read()) != -1) {
+                baos.write(255 - b);
+            }
+            byte[] bytes = baos.toByteArray();
+            return defineClass(name, bytes, 0, bytes.length);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return super.findClass(name);
+    }
+
+    public static void main(String[] args) {
+        CustomClassloader customClassloader = new CustomClassloader();
+        Class clazz;
+        try {
+            clazz = customClassloader.findClass("Hello");
+            Method hello = clazz.getMethod("hello");
+            hello.invoke(clazz.newInstance(), (Object[]) null);
+        } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
